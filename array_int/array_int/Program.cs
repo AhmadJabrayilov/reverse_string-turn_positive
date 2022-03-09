add955dba9715f0fbf43d782029326b0d156c948
AhmadJabrayilov <tu25tyfpp@code.edu.az>
2022-03-09 22:19:29 +0400

    feature array_int, reverse_string files

---
+++ array_int/array_int/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace array_int
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 5, -2, 10, -5, 15 };
+            turn_positive(nums);
+           
+
+        }
+
+        #region array_int
+        static int[] turn_positive(int[] numbers)
+        {
+             for(int i = 0; i<numbers.Length; i++)
+            {
+                if (numbers[i]<0)
+                {
+                    numbers[i] *= -1;
+                }
+            Console.Write(numbers[i]+" ");
+            }
+            return numbers;
+        }
+
+
+        #endregion
+    }
+}
---
+++ reverse_string/reverse_string/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace reverse_string
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("enter word: ");
+            string new_word =  Console.ReadLine();
+            Console.Write("reverse word: ");
+            rvs_string(new_word);
+        }
+        #region rvs_string
+        static string rvs_string(string word )
+        {
+            
+            
+            for (int i = word.Length-1; i>=0; i--)
+            {
+                Console.Write(word[i]);
+            }
+            return word;
+
+        }
+        #endregion
+    }
+}
